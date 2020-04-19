414e639d8d2fd1e79d7ce145f87eaccac3cbd48e
Kenneth Mui <klmui@wisc.edu>
2020-04-19 02:53:12 -0500

    Get DB connected and add post functionality

--- functions/APIs/cats.js
+++ functions/APIs/cats.js
@@ -1,11 +1,14 @@
+const { db } = require('../util/admin');
+
 exports.getRandomCat = (request, response) => {
-  const cats = [
-    "Art by Hayley Jane Wakenshaw<br/>" +
-    "  ^~^  ,<br/>" +
-    " ('Y') )<br/>" +
-    " /   \\/ <br/>" +
-    "(\\|||/) hjw<br/>",
-    '2'
-  ]
+  // const cats = [
+  //   "Art by Hayley Jane Wakenshaw<br/>" +
+  //   "  ^~^  ,<br/>" +
+  //   " ('Y') )<br/>" +
+  //   " /   \\/ <br/>" +
+  //   "(\\|||/) hjw<br/>",
+  //   '2'
+  // ]
+
   return response.send("<span style='font-size:large;letter-spacing:3.5px'>" + cats[Math.floor(Math.random() * cats.length)].replace(/ /g, "&nbsp").replace(/\\/g, "\\\\")) + "</span>";
 }
--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,34 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 
+const { db } = require('./util/admin');
+
 const {
     getRandomCat
 } = require('./APIs/cats')
 
+// GET a cat
 app.get('/', getRandomCat);
+
+// POST a cat
+app.post('/', (req, res) => {
+  const newCat = {
+    cat: req.body.cat
+  }
+  db
+    .collection('cats')
+    .add(newCat)
+    .then((doc) => {
+        const responseCatItem = newCat;
+        responseCatItem.id = doc.id;
+        return res.json(responseCatItem);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: 'Something went wrong' });
+      console.log(err);
+    });
+});
+
 exports.api = functions.https.onRequest(app);
 
 // exports.helloWorld = functions.https.onRequest((request, response) => {
